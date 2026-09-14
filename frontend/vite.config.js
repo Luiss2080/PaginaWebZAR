f4488202ab282e339c4f64d149392317b956bb9e
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2026-09-14 09:49:40 -0400

    Refactor code structure for improved readability and maintainability

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,10 +11,6 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
-      '/img': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
     },
   },
 })
